340ff213af96285026a2b7388684b904433c3057
Advaith Ganesan <133765748+advaithganesan@users.noreply.github.com>
2025-04-02 12:51:25 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,19 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+# Addition
+Addition = a + b
+
+# Subtraction
+Subtraction = a - b
+
+# Multiplication
+Multiplication = a * b
+
+# Division
+Division = a/b
+
+print("Addition:", Addition)
+print("Subtraction:", Subtraction)
+print("Multiplication:", Multiplication)
+print("Division:", Division)
+
---
+++ Task 2.py
@@ -0,0 +1,7 @@
+a = input("Enter your First Name: ")
+b = input("Enter your Second Name: ")
+# Concatenate first and last names
+full_name = f"{a} {b}"
+
+# Print the personalized greeting
+print(f"Hello, {full_name}! Welcome to Python Program!")
